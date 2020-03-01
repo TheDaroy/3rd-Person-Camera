6d0a01216529e10a5a64fc840b3246f8726ba92c
daroy <thedaroy@live.com>
2020-03-01 20:29:10 +0100

    no message

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject masterObject;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = FindObjectOfType<Camera>();       
+        if (masterObject) { this.transform.SetParent(masterObject.transform);}
+        else { Debug.LogError(this + " No MasterObject Found"); }
+    }
     void Start()
     {
+        Vector3 startPosition = masterObject.transform.position;
+        startPosition.x -= 30;
+        transform.position = startPosition;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.RotateAround(masterObject.transform.localPosition, Vector3.left, 30 * Input.GetAxis("Mouse X"));
+        //transform.RotateAround(masterObject.transform.localPosition, Vector3.up, 30 * Input.GetAxis("Mouse Y"));
         
     }
 }
